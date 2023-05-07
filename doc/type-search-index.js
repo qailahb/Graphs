3564b6e28bb624ddd62beee187b17f3bb21f9dce
Qailah <qailahb@gmail.com>
2023-05-07 18:57:09 +0200

    Add docs

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Generator"},{"p":"<Unnamed>","l":"Graph"},{"p":"<Unnamed>","l":"Program"}];updateSearchResults();
